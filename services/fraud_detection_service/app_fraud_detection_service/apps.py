4948a019ec5e0d1995f87137549c5fef7f2b587a
AkmaLeyzal <akmaleyzal@gmail.com>
2024-09-29 18:41:21 +0700

    Start django app

---
+++ services/fraud_detection_service/app_fraud_detection_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppFraudDetectionServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_fraud_detection_service'
---
+++ services/payment_service/app_payment_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppPaymentServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_payment_service'
