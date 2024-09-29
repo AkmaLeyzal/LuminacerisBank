a0315334bd601b6edb43f3f168c71fbca82a1515
AkmaLeyzal <akmaleyzal@gmail.com>
2024-09-29 20:50:10 +0700

    Update django app

--- services/account_service/account_service/settings.py
+++ services/account_service/account_service/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
