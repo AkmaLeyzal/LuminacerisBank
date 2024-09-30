e851905e8ddc867a3d223365c43c4bab10735162
AkmaLeyzal <akmaleyzal@gmail.com>
2024-09-30 13:25:21 +0700

    Update apps.py

--- services/loan_service/loan/apps.py
+++ services/loan_service/loan/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppLoanServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_loan_service'
+    name = 'loan'
