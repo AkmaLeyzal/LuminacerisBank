6cfd68eacc3c20326ad44a9289b9c0eeb721b485
AkmaLeyzal <akmaleyzal@gmail.com>
2024-10-09 17:54:36 +0700

    Rename app

--- services/auth_service/backend/authentication/apps.py
+++ services/auth_service/backend/authentication/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppAuthServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_auth_service'
+    name = 'authentication'
